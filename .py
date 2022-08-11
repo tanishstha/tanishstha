28631c3b19cbb25a5078b85a4bb40be8a233df1b
unknown <tkhoju@gmail.com>
2022-08-11 12:11:15 +0545

    about string in python

---
+++ .py
@@ -0,0 +1,12 @@
+# --> It represents a one line comment in python
+
+'''
+it represents a multi-line comment
+we can write anything here
+'''
+a=5 #here,a is a variable which stores/holds integer type value i.e 5
+naam="tanish" #here, naam is a variable which stores/holds string type value i.e "tanish"
+
+
+
+
